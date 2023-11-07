ab67c3af57b4c24e0b47d6fee44a5e8614fa526b
teo <hoangnm.ndm@gmail.com>
2023-11-07 15:54:45 +0700

    demo_type

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const age: number = 30;
+function greet(person: string): void {
+  console.log(`Xin chao ${person}, anh nam nay ${age} co phai khong?`);
 }
 
-export default App
+const myName = "hoangnm";
+console.log(typeof myName);
+
+// Ép kiểu:
+const yourName = "Hoang";
+
+yourName as string; // ép kiểu sử dụng as
+
+function login(isLoggin: boolean) {
+  if (isLoggin) {
+    console.log("Da dang nhap thanh cong!");
+  } else {
+    console.log("Ban chua dang nhap!");
+  }
+}
+
+function App() {
+  return (
+    <>
+      <button onClick={() => greet("Hoang")}>An vao day</button>
+      <button onClick={() => login(true)}>Login</button>
+    </>
+  );
+}
+
+export default App;
