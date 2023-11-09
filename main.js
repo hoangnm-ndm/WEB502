53a7c10d9142b35c87be61603dd399a675c9582d
teo <hoangnm.ndm@gmail.com>
2023-11-09 16:54:05 +0700

    Day3: add Type

---
+++ main.js
@@ -0,0 +1,23 @@
+// type
+{
+    var myName = "Hoang";
+    console.log(typeof myName);
+    var myAge = 20;
+    var isLoggin = true;
+    function sum(a, b) {
+        // return a + b;
+        console.log(a + b);
+    }
+    var myInfor = {
+        name: "Hoang",
+        age: 20,
+    };
+    var myStudents = ["hoang", "dung", "chuc"];
+    var myStudents2 = ["hoang", "dung", "chuc"];
+    var myTuple = ["hoang", 20];
+    var myUnion = // Dữ liệu kiểu liên hiệp
+     void 0; // Dữ liệu kiểu liên hiệp
+    myUnion = "hoang";
+    myUnion = 20;
+    myUnion = true;
+}
---
+++ main.ts
@@ -0,0 +1,27 @@
+// type
+{
+  const myName = "Hoang";
+  console.log(typeof myName);
+  const myAge: number = 20;
+  const isLoggin: boolean = true;
+  function sum(a: number, b: number): void {
+    // return a + b;
+    console.log(a + b);
+  }
+
+  const myInfor: {
+    name: string;
+    age: number;
+  } = {
+    name: "Hoang",
+    age: 20,
+  };
+
+  const myStudents: String[] = ["hoang", "dung", "chuc"];
+  const myStudents2: Array<string> = ["hoang", "dung", "chuc"];
+  const myTuple: [string, number] = ["hoang", 20];
+  let myUnion: string | number | boolean; // Dữ liệu kiểu liên hiệp
+  myUnion = "hoang";
+  myUnion = 20;
+  myUnion = true;
+}
