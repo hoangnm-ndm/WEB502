f7693c714ca5c4fc6a436216cb679caefc327348
teo <hoangnm.ndm@gmail.com>
2023-11-09 18:31:38 +0700

    add tsconfig.json

---
+++ src/main.ts
@@ -0,0 +1,34 @@
+// type;
+
+const myName = "Hoang";
+console.log(typeof myName);
+const myAge: number = 20;
+const isLoggin: boolean = true;
+function sum(a: number, b: number): void {
+  // return a + b;
+  console.log(a + b);
+}
+
+const myInfor: {
+  name: string;
+  age: number;
+} = {
+  name: "Hoang",
+  age: 20,
+};
+
+const myStudents: String[] = ["hoang", "dung", "chuc"];
+const myStudents2: Array<string> = ["hoang", "dung", "chuc"];
+const myTuple: [string, number] = ["hoang", 20];
+let myUnion: string | number | boolean; // Dữ liệu kiểu liên hiệp
+myUnion = "hoang";
+myUnion = 20;
+myUnion = true;
+
+enum ROLE {
+  USER,
+  ADMIN = 0,
+  MANAGER,
+}
+
+console.log(ROLE.MANAGER);
