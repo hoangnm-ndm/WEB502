161ebee7ce92c7e06c87241ca110532c3534fa1f
hoangnm.ndm <hoangnm.ndm@gmail.com>
2023-11-23 15:53:23 +0700

    add CRUD

---
+++ src/components/ProductForm.tsx
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from "react";
+
+type ProductFormProps = {
+  onClose: () => void; // Hàm đóng form
+  onSubmit: (product: Product) => void; // Hàm gửi dữ liệu sản phẩm lên server
+  initialProduct?: Product | null; // Sản phẩm ban đầu khi chỉnh sửa
+};
+
+type Product = {
+  name: string;
+  price: number;
+  desc: string;
+  _id?: string;
+};
+
+const ProductForm: React.FC<ProductFormProps> = ({
+  onClose,
+  onSubmit,
+  initialProduct,
+}) => {
+  const [product, setProduct] = useState<Product>(
+    initialProduct || {
+      name: "",
+      price: 0,
+      desc: "",
+    }
+  );
+
+  useEffect(() => {
+    if (initialProduct) {
+      setProduct(initialProduct);
+    }
+  }, [initialProduct]);
+
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setProduct((prevProduct) => ({ ...prevProduct, [name]: value }));
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    onSubmit(product);
+  };
+
+  return (
+    <div>
+      <h2>{initialProduct ? "Edit Product" : "Add Product"}</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input
+            type="text"
+            name="name"
+            value={product.name}
+            onChange={handleInputChange}
+          />
+        </label>
+        <br />
+        <label>
+          Price:
+          <input
+            type="number"
+            name="price"
+            value={product.price}
+            onChange={handleInputChange}
+          />
+        </label>
+        <br />
+        <label>
+          Description:
+          <textarea
+            name="desc"
+            value={product.desc}
+            onChange={handleInputChange}
+          />
+        </label>
+        <br />
+        <button type="submit">{initialProduct ? "Update" : "Add"}</button>
+        <button type="button" onClick={onClose}>
+          Cancel
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default ProductForm;
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import ProductForm from "./ProductForm";
 
 type Product = {
   name: string;
@@ -9,41 +11,116 @@ type Product = {
 
 const ProductList = () => {
   const [products, setProducts] = React.useState<Product[]>([]);
-  React.useEffect(() => {
-    fetch("http://localhost:8000/api/products")
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data.data);
-        setProducts(data.data);
+  const [isFormOpen, setIsFormOpen] = useState(false);
+  const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/api/products")
+      .then((response: any) => {
+        console.log(response.data.data);
+        setProducts(response.data.data);
+      })
+      .catch((error: any) => {
+        console.error("Error fetching products:", error);
       });
   }, []);
 
   const deleteProduct = (_id: string) => {
-    console.log(_id);
+    axios
+      .delete(`http://localhost:8000/api/products/${_id}`)
+      .then(() => {
+        const updatedProducts = products.filter(
+          (product) => product._id !== _id
+        );
+        setProducts(updatedProducts);
+      })
+      .catch((error: any) => {
+        console.error("Error deleting product:", error);
+      });
   };
 
-  const updateProduct = (product: Product) => {
-    console.log(product._id);
+  const updateProduct = (updatedProduct: Product) => {
+    axios
+      .put(
+        `http://localhost:8000/api/products/${updatedProduct._id}`,
+        updatedProduct
+      )
+      .then((response: any) => {
+        const updatedProducts = products.map((product) =>
+          product._id === updatedProduct._id ? response.data.data : product
+        );
+        setProducts(updatedProducts);
+      })
+      .catch((error: any) => {
+        console.error("Error updating product:", error);
+      });
   };
+
+  const addProduct = (newProduct: Product) => {
+    axios
+      .post("http://localhost:8000/api/products", newProduct)
+      .then((response: any) => {
+        setProducts([...products, response.data.data]);
+      })
+      .catch((error: any) => {
+        console.error("Error adding product:", error);
+      });
+  };
+
+  const closeForm = () => {
+    setIsFormOpen(false);
+    setSelectedProduct(null);
+  };
+
+  const openForm = (product: Product | null = null) => {
+    setSelectedProduct(product);
+    setIsFormOpen(true);
+  };
+
+  const submitForm = (product: Product) => {
+    if (selectedProduct) {
+      updateProduct(product);
+    } else {
+      addProduct(product);
+    }
+    closeForm();
+  };
+
   return (
-    <>
-      <h1>Product List</h1>
-      <ul>
-        {products.map((product) => (
-          <li key={product._id}>
-            <h3>{product.name}</h3>
-            <p>{product.desc}</p>
-            <p>{product.price}</p>
-            <p>
-              <button onClick={() => product._id && deleteProduct(product._id)}>
-                Delete
-              </button>
-              <button onClick={() => updateProduct(product)}>Update</button>
-            </p>
-          </li>
-        ))}
-      </ul>
-    </>
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          {isFormOpen && (
+            <ProductForm
+              onClose={closeForm}
+              onSubmit={submitForm}
+              initialProduct={selectedProduct}
+            />
+          )}
+        </div>
+        <div className="col">
+          <h1>Product List</h1>
+          <button onClick={() => openForm()}>Add Product</button>
+          <ul>
+            {products.map((product) => (
+              <li key={product._id}>
+                <h3>{product.name}</h3>
+                <p>{product.desc}</p>
+                <p>{product.price}</p>
+                <p>
+                  <button
+                    onClick={() => product._id && deleteProduct(product._id)}
+                  >
+                    Delete
+                  </button>
+                  <button onClick={() => openForm(product)}>Update</button>
+                </p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
   );
 };
 
